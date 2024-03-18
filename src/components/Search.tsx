3677694f10425b0c28823fc4ca3823e4e1d3cbf0
Denzel Huijbers <huijbers15@outlook.com>
2024-03-18 18:16:35 +0100

    added search component, fixed tailwind omport

---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import React, { useState, ChangeEvent } from "react";
+import { IoIosSearch } from "react-icons/io";
+
+interface SearchBarProps {
+    onSearch: (searchTerm: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setSearchTerm(value);
+        onSearch(value);
+    };
+
+    return (
+        <div className="flex flex-row bg-white justify-center items-center  focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-1 px-2 w-full appearance-none leading-normal ">
+            <input
+                className="bg-white text-black outline-none"
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleChange}
+            />
+            <IoIosSearch className="fill-black" />
+        </div>
+    );
+};
+
+export default SearchBar;

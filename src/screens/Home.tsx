8e17003094e2ab1f7a0b62288a61885400ac7e49
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-03-21 12:47:03 -0300

    correcting eslint

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+// import {Link} from "react-router-dom";
+
+import CarouselComponent from "../components/CarouselComponent.tsx";
+
+export default function Home() {
+    return (
+        <>
+            <h1>You are in the home page</h1>
+            <CarouselComponent />
+        </>
+    );
+}

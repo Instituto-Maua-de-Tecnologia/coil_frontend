3f97f450c944fd7b164a2a77c8d648099b8115ff
Felipe Carillo <felipecarillo@outlook.com>
2024-03-22 08:12:06 -0300

    New test for CD

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from '@aws-cdk/aws-amplify';
+import * as codecommit from '@aws-cdk/aws-codecommit'
 
 export class IacStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,16 +12,23 @@ export class IacStack extends cdk.Stack {
       "AZURE_CLIENT_ID": process.env.AZURE_CLIENT_ID || "",
     }
 
+    const sampleRepo = new codecommit.Repository(
+      this,
+      "CoilFrontendRepo",
+      {
+        repositoryName: "coil-frontend-repo-" + ENVIROMENT_VARIABLES.STAGE,
+        description:
+          "Repository for the frontend of the Coil project",
+      }
+    )
+
     const amplifyApp = new amplify.App(this, 'coil_froentend', {
-      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
-        owner: ENVIROMENT_VARIABLES.OWNER,
-        repository: 'coil-frontend',
-        oauthToken: cdk.SecretValue.secretsManager('coil-github-token')
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: sampleRepo,
       }),
-      environmentVariables: ENVIROMENT_VARIABLES,
+      environmentVariables: ENVIROMENT_VARIABLES
     });
 
-    const prodBranch = amplifyApp.addBranch('prod');
-    const devBranch = amplifyApp.addBranch('dev');
+    const branch = amplifyApp.addBranch(ENVIROMENT_VARIABLES.STAGE);
   }
 }

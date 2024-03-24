131129f6f2b1932d129f1bfd5e66d051059f4b35
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-03-24 18:38:20 -0300

    adding carousel, changing site logo from user's browser theme, and restructuring routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./screens/Home.tsx";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import AppRoutes from "./Routes";
+
+const useThemeDetector = () => {
+    const getWatchMedia = () =>
+        window.matchMedia("(prefers-color-scheme: dark)");
+    const [isDarkTheme, setIsDarkTheme] = useState(getWatchMedia().matches);
+
+    const mqListener = (e: MediaQueryListEvent) => {
+        setIsDarkTheme(e.matches);
+    };
+
+    useEffect(() => {
+        const mq = getWatchMedia();
+        mq.addListener(mqListener); // best practice is use addEventListener instead of addListener, but when we change for addEventListener the page doesn't update automatically
+        return () => mq.removeListener(mqListener);
+    }, []);
+    return isDarkTheme;
+};
 
 export default function App() {
+    const isDarkTheme = useThemeDetector();
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            {createPortal(
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href={
+                        isDarkTheme
+                            ? "/maua-fontys-light.svg"
+                            : "maua-fontys-dark.svg"
+                    }
+                />,
+                document.getElementById("link-by-user-theme") as
+                    | Element
+                    | DocumentFragment
+            )}
+            <AppRoutes />
+        </>
     );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,10 @@
-// import {Link} from "react-router-dom";
-
-import Carousel from "../components/Carousel.tsx";
+import CarouselComponent from "../components/Carousel.tsx";
 
 export default function Home() {
     return (
         <>
-            <h1>You are in the home page</h1>
-            <Carousel />
+            <h1>You are in the Home page</h1>
+            <CarouselComponent />
         </>
     );
 }

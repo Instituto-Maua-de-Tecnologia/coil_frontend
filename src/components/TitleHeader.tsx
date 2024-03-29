90a4506ade140a32ac94880c7a2e51053e34a2ce
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-03-29 19:50:11 -0300

    responsive header

--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -4,9 +4,9 @@ interface TitleHeaderProps {
 
 export default function TitleHeader({ title }: TitleHeaderProps) {
     return (
-        <div className="w-[1861px] mx-auto mt-4 h-[84px]">
-            <div className="w-[1863px] h-[84px] top-0 left-0">
-                <div className="relative w-[1861px] h-[84px] bg-[#2684ff] rounded-[24px]">
+        <div className="w-full mx-auto mt-4 h-[84px]">
+            <div className="w-full h-[84px] top-0 left-0">
+                <div className="relative w-[95%] mx-auto h-[84px] bg-[#2684ff] rounded-[24px]">
                     <div className="absolute top-[20px] left-[50px] [font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-[#f9fafc] text-[36px] tracking-[-1.08px] leading-[normal]">
                         {title}
                     </div>

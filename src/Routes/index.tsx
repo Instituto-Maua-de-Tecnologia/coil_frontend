0fba469821951a731f97d25d374c25d5f4652d35
Alessandro Lima <78627928+alexZ7000@users.noreply.github.com>
2024-04-03 08:24:09 -0300

    resolving index.tsx conflict

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../screens/Home.tsx";
+import Institutions from "../screens/Enrolled.tsx";
+import Enrolled from "../screens/Enrolled.tsx";
+import Projects from "../screens/Projects.tsx";
+import Results from "../screens/Results.tsx";
+import User from "../screens/User.tsx";
+import Signout from "../screens/Signout.tsx";
 import Hero from "../components/Hero.tsx";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/landing" element={<Hero />} />
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/Institutions"} element={<Institutions />} />
+                <Route path={"/Projects"} element={<Projects />} />
+                <Route path={"/Enrolled"} element={<Enrolled />} />
+                <Route path={"/Results"} element={<Results />} />
+                <Route path={"/User"} element={<User />} />
+                <Route path={"/Signout"} element={<Signout />} />
+                <Route path={"/landing"} element={<Hero />} />
             </Routes>
         </BrowserRouter>
     );

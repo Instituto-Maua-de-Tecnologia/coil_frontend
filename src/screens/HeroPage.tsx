9fc63276d6988292ced91013d9291f4c94c42e0a
Denzel Huijbers <huijbers15@outlook.com>
2024-04-16 13:24:51 +0200

    added title

--- src/screens/HeroPage.tsx
+++ src/screens/HeroPage.tsx
@@ -7,11 +7,16 @@ export default function HeroPage() {
     return (
         <>
             <Hero />
-            <ImageTrack direction="right" speed={10} className="my-5">
-                {scrollTrackItems.map((trackItem, index) => (
-                    <TrackItem key={index} trackItem={trackItem} />
-                ))}
-            </ImageTrack>
+            <div className="mt-10">
+                <h1 className="text-center text-lg font font-semibold text-blue-500 my-2">
+                    Our Partners
+                </h1>
+                <ImageTrack direction="right" speed={10} className="my-5">
+                    {scrollTrackItems.map((trackItem, index) => (
+                        <TrackItem key={index} trackItem={trackItem} />
+                    ))}
+                </ImageTrack>
+            </div>
         </>
     );
 }

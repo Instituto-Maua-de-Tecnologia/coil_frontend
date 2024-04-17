0cca8c76568ba22fdc81719e5e365c4587e7d18c
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-04-17 18:45:09 -0300

    removing carousel from home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,3 @@
-import CarouselComponent from "@components/Carousel.tsx";
 import ProjectList from "@components/ProjectList.tsx";
 import TitleHeader from "@components/TitleHeader.tsx";
 import SideBar from "@components/SideBar.tsx";
@@ -10,10 +9,7 @@ export default function Home() {
                 <TitleHeader title={"Home"} className={""} />
                 <div className="flex flex-row">
                     <SideBar />
-                    <div className="pl-[2.5rem] overflow-hidden">
-                        <ProjectList />
-                        <CarouselComponent />
-                    </div>
+                    <ProjectList />
                 </div>
             </div>
         </>

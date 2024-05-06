c27155aed5ec4e5afb4641af8b25cf153b65b730
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-05-06 11:34:04 -0300

    correcting get-activities and get-all-activities routes and connection with the signup page

--- src/integrations/modules/activity/get_all_activities.ts
+++ src/integrations/modules/activity/get_all_activities.ts
@@ -1,35 +1,80 @@
 import axios, { AxiosError } from "axios";
 
-interface GetAllActivitiesProps {
-    activity_id: string;
+interface GetActivityProps {
+    type_activity: string;
 }
 
-interface GetAllActivitiesResponse {}
+interface GetActivityResponse {
+    message: "string";
+    data: [
+        {
+            id?: "string";
+            title?: "string";
+            start_date?: "string";
+            end_date?: "string";
+            created_at?: "string";
+            updated_at?: "string";
+            courses?: [
+                {
+                    course_id?: number;
+                    course?: {
+                        name: "string";
+                    };
+                }
+            ];
+            languages?: [
+                {
+                    language?: "string";
+                }
+            ];
+            partner_institutions?: [
+                {
+                    institution_id?: "string";
+                    institution?: {
+                        name: "string";
+                        images: ["string"];
+                    };
+                }
+            ];
+            activity_status?: {
+                id: number;
+                name: "string";
+            };
+            activity_type?: {
+                id: number;
+                name: "string";
+            };
+        }
+    ];
+}
 
-export default async function getActivity(props: GetAllActivitiesProps) {
+export default async function getAllActivities(props: GetActivityProps) {
     const token = localStorage.getItem("token");
     return new Promise((resolve, reject) => {
         const endpoint: string = import.meta.env.VITE_ENDPOINT as string;
         axios
-            .get(`${endpoint}/get-activity?activity_id=${props.activity_id}`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: token as string
+            .get(
+                `${endpoint}/get-all-activities?type_activity=${props.type_activity}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: token as string
+                    }
                 }
-            })
+            )
             .then((response) => {
-                const responseData: GetAllActivitiesResponse =
-                    response.data as GetAllActivitiesResponse;
-                resolve(responseData);
+                const responseData: GetActivityResponse =
+                    response.data as GetActivityResponse;
+                resolve(responseData.data);
             })
             .catch((error: AxiosError) => {
-                const convertedError: AxiosError<GetAllActivitiesResponse> =
-                    error as AxiosError<GetAllActivitiesResponse>;
+                const convertedError: AxiosError<GetActivityResponse> =
+                    error as AxiosError<GetActivityResponse>;
                 const errorResponse = convertedError.response;
                 if (errorResponse) {
                     reject({
                         status: errorResponse.status,
-                        message: errorResponse.data
+                        message: errorResponse.data.message
                     });
                 }
             });

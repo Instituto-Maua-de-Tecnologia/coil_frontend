6023127f2a463254d458ac6c873d017d6bd2c3f6
luiz-felipe-b <lfbalaminute@hotmail.com>
2024-05-05 21:55:16 -0300

    create project screen static complete

--- src/components/CreateProjectForm.tsx
+++ src/components/CreateProjectForm.tsx
@@ -2,88 +2,7 @@ import Select, { StylesConfig } from "react-select";
 import TrashButton from "./TrashButton";
 import DropdownIndicator from "./DropdownIndicator";
 import React, { useState } from "react";
-
-const inputStyle = {
-    backgroundColor: "#F0F3FB",
-    border: "none",
-    borderRadius: "30px",
-    padding: "9px 12px",
-    outline: "none"
-};
-
-const textAreaStyle = {
-    backgroundColor: "#F0F3FB",
-    border: "none",
-    borderRadius: "18px",
-    padding: "8px 12px",
-    outline: "none",
-    resize: "none"
-};
-
-const customStyles: StylesConfig = {
-    control: (provided) => ({
-        ...provided,
-        backgroundColor: "#F0F3FB",
-        border: "none",
-        boxShadow: "none",
-        borderRadius: "30px"
-    }),
-    menu: () => ({
-        backgroundColor: "#F0F3FB",
-        padding: "8px",
-        borderRadius: "30px",
-        boxShadow: "0 0 2px #1D232C50"
-    }),
-    option: (provided, state) => ({
-        ...provided,
-        margin: "0px 0px 8px 0px",
-        maxWidth: "240px",
-        color: "#FFFFFF",
-        borderRadius: "30px",
-        backgroundColor: state.isFocused ? "#673366" : "#512650",
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis"
-    }),
-    multiValue: (provided) => ({
-        ...provided,
-        color: "#FFFFFF",
-        padding: "1px 4px",
-        borderRadius: "30px",
-        backgroundColor: "#673366",
-        maxWidth: "240px",
-        fontSize: "20px",
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis"
-    }),
-    multiValueLabel: (provided) => ({
-        ...provided,
-        color: "#FFFFFF"
-    }),
-    multiValueRemove: (provided) => ({
-        ...provided,
-        color: "#FFFFFF", // Defina a cor desejada para o ícone de remoção
-        ":hover": {
-            backgroundColor: "transparent" // Remova o fundo ao passar o mouse
-        }
-    }),
-    singleValue: (provided) => ({
-        ...provided,
-        color: "#FFFFFF",
-        padding: "4px 12px",
-        borderRadius: "30px",
-        backgroundColor: "#673366"
-    }),
-    dropdownIndicator: (provided) => ({
-        ...provided,
-        color: "#1D232C"
-    }),
-    placeholder: (provided) => ({
-        ...provided,
-        color: "#CBD0DD"
-    })
-};
+import { useThemeDetector } from "@util/ThemeDetector";
 
 const langOptions = [
     { value: "portuguese", label: "Portuguese" },
@@ -107,6 +26,128 @@ const projectStatusOptions = [
 ];
 
 const CreateProjectForm: React.FC = () => {
+    const isDarkTheme = useThemeDetector();
+
+    const inputStyle = {
+        backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+        placeholderColor: isDarkTheme ? "#0F1820" : "CBD0DD",
+        border: "none",
+        borderRadius: "30px",
+        padding: "9px 12px",
+        outline: "none"
+    };
+
+    const textAreaStyle = {
+        backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+        border: "none",
+        borderRadius: "18px",
+        padding: "8px 12px",
+        outline: "none",
+        resize: "none"
+    };
+
+    const multiStyle: StylesConfig = {
+        control: (provided) => ({
+            ...provided,
+            backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+            border: "none",
+            boxShadow: "none",
+            borderRadius: "30px"
+        }),
+        menu: () => ({
+            backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+            padding: "8px",
+            borderRadius: "30px",
+            boxShadow: "0 0 2px #1D232C50"
+        }),
+        option: (provided, state) => ({
+            ...provided,
+            margin: "0px 0px 8px 0px",
+            maxWidth: "240px",
+            width: "100%",
+            color: "#FFFFFF",
+            borderRadius: "30px",
+            backgroundColor: state.isFocused ? "#673366" : "#512650",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis"
+        }),
+        multiValue: (provided) => ({
+            ...provided,
+            color: "#FFFFFF",
+            padding: "1px 4px",
+            borderRadius: "30px",
+            backgroundColor: "#673366",
+            maxWidth: "240px",
+            fontSize: "20px",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis"
+        }),
+        multiValueLabel: (provided) => ({
+            ...provided,
+            color: "#FFFFFF"
+        }),
+        multiValueRemove: (provided) => ({
+            ...provided,
+            color: "#FFFFFF",
+            ":hover": {
+                backgroundColor: "transparent"
+            }
+        }),
+        dropdownIndicator: (provided) => ({
+            ...provided,
+            color: isDarkTheme ? "#FFFFFF" : "#1D232C"
+        }),
+        placeholder: (provided) => ({
+            ...provided,
+            color: isDarkTheme ? "#0F1820" : "#CBD0DD"
+        })
+    };
+
+    const singleStyle: StylesConfig = {
+        control: (provided) => ({
+            ...provided,
+            backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+            border: "none",
+            boxShadow: "none",
+            borderRadius: "30px"
+        }),
+        menu: () => ({
+            backgroundColor: isDarkTheme ? "#223A4F" : "#F0F3FB",
+            padding: "8px",
+            borderRadius: "30px",
+            boxShadow: "0 0 2px #1D232C50"
+        }),
+        option: (provided) => ({
+            ...provided,
+            margin: "0px 0px 8px 0px",
+            width: "100%",
+            background: "transparent",
+            color: isDarkTheme ? "#FFFFFF" : "#1D232C",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            borderRadius: "30px",
+            textOverflow: "ellipsis"
+        }),
+        singleValue: (provided) => ({
+            ...provided,
+            color: isDarkTheme ? "#FFFFFF" : "#1D232C",
+            width: "100%",
+            background: "transparent",
+            padding: "4px 12px",
+            borderRadius: "30px"
+        }),
+        dropdownIndicator: (provided) => ({
+            ...provided,
+            fill: "#FFFFFF"
+        }),
+        placeholder: (provided) => ({
+            ...provided,
+            color: isDarkTheme ? "#0F1820" : "#CBD0DD"
+        })
+    };
+
     const [criterias, setCriterias] = useState<string[]>([""]);
 
     const handleAddCriteria = () => {
@@ -120,19 +161,21 @@ const CreateProjectForm: React.FC = () => {
     };
 
     return (
-        <div className="w-full ml-4 px-7 py-4 bg-sb-bg rounded-3xl">
+        <div
+            className={`w-full md:ml-4 px-4 py-4 ${isDarkTheme ? "bg-[#14222E]" : "bg-[#FFFFFF]"} rounded-3xl overflow-auto`}
+        >
             <div className="form [font-family:'Inter',Helvetica] font-semibold space-y-2">
-                <div className="form-row-1 flex flex-col mx-2 max-w-[50%] min-w-[420px]">
+                <div className="form-row-1 flex flex-col mx-2 max-w-[49%] min-w-[420px] space-y-2">
                     <label htmlFor="projectName">Project Name</label>
                     <input
-                        className="placeholder:text-[#CBD0DD]"
+                        className={`placeholder:text-[${inputStyle["placeholderColor"]}]`}
                         type="text"
                         placeholder="Type the name..."
                         style={inputStyle}
                     />
                 </div>
-                <div className="form-row-2 flex min-w-[320px]">
-                    <div className="langMultiSelect flex flex-col mx-2 w-full">
+                <div className="form-row-2 md:flex min-w-[320px]">
+                    <div className="langMultiSelect flex flex-col mx-2 w-full space-y-2">
                         <label htmlFor="languages">Languages</label>
                         <Select
                             isMulti
@@ -142,10 +185,10 @@ const CreateProjectForm: React.FC = () => {
                                 IndicatorSeparator: () => null,
                                 DropdownIndicator: () => <DropdownIndicator />
                             }}
-                            styles={customStyles}
+                            styles={multiStyle}
                         />
                     </div>
-                    <div className="courseMultiSelect flex flex-col w-full mx-2">
+                    <div className="courseMultiSelect flex flex-col w-full mx-2 space-y-2">
                         <label htmlFor="selectableCourses">
                             Selectable Courses
                         </label>
@@ -154,7 +197,7 @@ const CreateProjectForm: React.FC = () => {
                             menuPosition="fixed"
                             isClearable={true}
                             isSearchable={true}
-                            styles={customStyles}
+                            styles={multiStyle}
                             components={{
                                 IndicatorSeparator: () => null,
                                 DropdownIndicator: () => <DropdownIndicator />
@@ -163,9 +206,9 @@ const CreateProjectForm: React.FC = () => {
                         />
                     </div>
                 </div>
-                <div className="form-row-3 flex min-w-[320px]">
+                <div className="form-row-3 md:flex min-w-[320px]">
                     <div className="form-row-3-1 flex w-full min-w-[50%]">
-                        <div className="partner-institution-select w-full mx-2">
+                        <div className="partner-institution-select w-full mx-2 space-y-2">
                             <label htmlFor="partnerInstitution">
                                 Partner Institution
                             </label>
@@ -178,10 +221,10 @@ const CreateProjectForm: React.FC = () => {
                                         <DropdownIndicator />
                                     )
                                 }}
-                                styles={customStyles}
+                                styles={singleStyle}
                             />
                         </div>
-                        <div className="project-status-select  w-full mx-2">
+                        <div className="project-status-select  w-full mx-2 space-y-2">
                             <label htmlFor="partnerInstitution">
                                 Project Status
                             </label>
@@ -194,29 +237,29 @@ const CreateProjectForm: React.FC = () => {
                                         <DropdownIndicator />
                                     )
                                 }}
-                                styles={customStyles}
+                                styles={singleStyle}
                             />
                         </div>
                     </div>
-                    <div className="form-row-3-2 flex min-w-[50%] w-full">
-                        <div className="application-start-date flex flex-col w-full mx-2">
+                    <div className="form-row-3-2 md:flex w-full">
+                        <div className="application-start-date flex flex-col mx-2 space-y-2 w-full">
                             <label htmlFor="applicationStartDate">
                                 Aplication Start Date
                             </label>
                             <input
-                                className="placeholder:text-[#CBD0DD]"
+                                className={`placeholder:text-[${inputStyle["placeholderColor"]}]`}
                                 type="text"
                                 placeholder="XX/XX/XXXX"
                                 style={inputStyle}
                             />
                         </div>
-                        <span className="arrow pt-7">{">"}</span>
-                        <div className="application-end-date flex flex-col w-full mx-2">
+                        <span className="arrow pt-10">{">"}</span>
+                        <div className="application-end-date flex flex-col mx-2 space-y-2 w-full">
                             <label htmlFor="applicationEndDate">
                                 Aplication End Date
                             </label>
                             <input
-                                className="placeholder:text-[#CBD0DD]"
+                                className={`placeholder:text-[${inputStyle["placeholderColor"]}]`}
                                 type="text"
                                 placeholder="XX/XX/XXXX"
                                 style={inputStyle}
@@ -224,27 +267,27 @@ const CreateProjectForm: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className="form-row-4 flex">
-                    <div className="project-description flex flex-col w-full mx-2">
+                <div className="form-row-4 md:flex">
+                    <div className="project-description flex flex-col w-full mx-2 space-y-2">
                         <label htmlFor="projectDescription">
                             Project Description
                         </label>
                         <textarea
-                            className="mt-2 placeholder:text-[#CBD0DD]"
+                            className={`mt-2 placeholder:text-[${inputStyle["placeholderColor"]}]`}
                             placeholder="Type the description..."
-                            rows={19}
+                            rows={18}
                             style={textAreaStyle}
                         />
                     </div>
-                    <div className="project-criteria mx-2 w-full">
+                    <div className="project-criteria mx-2 space-y-2 w-full">
                         <label htmlFor="projectCriteria">
                             Project Criteria
                         </label>
-                        <ul>
+                        <ul className="overflow-auto w-max-[460px]">
                             {criterias.map((c, index) => (
                                 <li
                                     key={index}
-                                    className="criteria-item flex items-center mt-2 mb-4"
+                                    className={`${c}      criteria-item flex items-center mt-1 mb-3`}
                                 >
                                     <TrashButton
                                         onRemoveCriteria={() => {
@@ -252,25 +295,16 @@ const CreateProjectForm: React.FC = () => {
                                         }}
                                     />
                                     <input
-                                        className="w-full ms-2 placeholder:text-[#CBD0DD]"
+                                        className={`w-full placeholder:text-[${inputStyle["placeholderColor"]}]`}
                                         type="text"
                                         placeholder="Type your criteria..."
                                         style={inputStyle}
                                     />
                                 </li>
                             ))}
-                            {/* <li className="criteria-item flex items-center mt-2">
-                                <TrashButton />
-                                <input
-                                    className="w-full ms-2"
-                                    type="text"
-                                    placeholder="Type your criteria..."
-                                    style={inputStyle}
-                                />
-                            </li> */}
                             <button
                                 onClick={handleAddCriteria}
-                                className="add-criteria text-[#2684FF] mt-2 text-semibold"
+                                className="add-criteria text-[#2684FF] text-semibold"
                             >
                                 + Add Criteria
                             </button>

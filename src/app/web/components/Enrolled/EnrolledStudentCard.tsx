a557e34f88f12fed9517d68c1eee8e29d5f65474
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-08-02 16:17:15 -0300

    fix view enrolled students

--- src/app/web/components/Enrolled/EnrolledStudentCard.tsx
+++ src/app/web/components/Enrolled/EnrolledStudentCard.tsx
@@ -1,34 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useThemeDetector } from "@functions/ThemeDetector.ts";
 import updateUserStatusInActivity from "@integrations/activity/admin&moderator/update_user_status_in_activity.ts";
 import { Button } from "primereact/button";
 import toast from "react-hot-toast";
 import IUser from "@interfaces/user/IUser.ts";
-import IStudent from "@interfaces/user/IStudent.ts";
 
-interface Applicants {
-    users_applicants: {
+interface ApplicantProps {
+    applicant: {
         id?: string;
         user?: IUser;
         status?: boolean;
-    }[];
+    };
     projectID: string;
 }
 
 export default function EnrolledStudentCard({
-    users_applicants,
+    applicant,
     projectID
-}: Applicants) {
+}: ApplicantProps) {
     const isDarkTheme = useThemeDetector();
-    const [applicants, setApplicants] =
-        useState<Applicants["users_applicants"]>(users_applicants);
+    const [status, setStatus] = useState<boolean | undefined>(applicant.status);
 
-    const handleToggleApprovation = (index: number, status: boolean) => {
-        setApplicants((prevApplicants) =>
-            prevApplicants.map((applicant, i) =>
-                i === index ? { ...applicant, status } : applicant
-            )
-        );
+    const handleToggleApprovation = (newStatus: boolean) => {
+        setStatus(newStatus);
     };
 
     const sendData = async () => {
@@ -36,9 +30,7 @@ export default function EnrolledStudentCard({
             updateUserStatusInActivity({
                 body: {
                     activity_id: projectID,
-                    applicants: applicants.map(
-                        (applicant) => applicant.id ?? ""
-                    )
+                    applicants: [applicant.id ?? ""]
                 }
             }),
             {
@@ -49,120 +41,54 @@ export default function EnrolledStudentCard({
         );
     };
 
-    function handleExportApprovedStudents() {
-        if (!applicants) {
-            throw new Error("No applicants found");
-        }
-        let data = [
-            {
-                name: "",
-                email: "",
-                RA: ""
-            }
-        ] as IStudent[];
-        applicants.map((applicant) => {
-            if (applicant.status === true) {
-                data = [
-                    {
-                        name: applicant.user?.name as string,
-                        email: applicant.user?.email as string,
-                        RA: applicant.user?.email.split("@")[0] as string
-                    }
-                ];
-            }
-        });
-
-        const headers = "Nome,RA,E-mail";
-        const csvRows = data.map(
-            (student) => `${student.name},${student.RA},${student.email}`
-        );
-        const csvContent = [headers, ...csvRows].join("\n");
-
-        const blob = new Blob([csvContent], { type: "text/csv" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "approved_students.csv";
-        a.click();
-        URL.revokeObjectURL(url);
-    }
-
     return (
-        <>
-            <div className={"text-end"}>
-                <Button
-                    className={"mt-1 shadow-md mb-3 right-0"}
-                    onClick={handleExportApprovedStudents}
-                >
-                    Export CSV
-                </Button>
-            </div>
-            <li
-                className={`sm:flex shadow-sm items-center ${isDarkTheme ? "bg-[#0F1820]" : "bg-[#F0F3FB]"} rounded-3xl p-4 mb-2 w-full`}
-            >
-                <div className="flex sm:relative sm:justify-between w-full">
-                    <div className="sm:flex w-full sm:items-center">
-                        {applicants.map((user_applicant, index) => (
-                            <React.Fragment key={"ResultsCard" + index}>
-                                <div className="font-bold w-full">
-                                    {user_applicant.user?.name}
-                                </div>
-                                <div className="font-bold w-full">
-                                    {user_applicant.user?.email.substring(
-                                        0,
-                                        user_applicant.user?.email.indexOf("@")
-                                    )}
-                                </div>
-                                <div className="font-bold w-full">
-                                    {user_applicant.user?.email}
-                                </div>
-                                <div className="mr-2 flex space-x-2">
-                                    <button
-                                        onClick={() =>
-                                            handleToggleApprovation(index, true)
-                                        }
-                                        className={`${
-                                            user_applicant.status
-                                                ? "bg-blue-600 text-white"
-                                                : "bg-gray-300 opacity-50 text-black"
-                                        } text-sm shadow-lg hover:opacity-80 transition-opacity duration-300 px-4 py-2 rounded-full`}
-                                    >
-                                        Approved
-                                    </button>
-                                    <button
-                                        onClick={() =>
-                                            handleToggleApprovation(
-                                                index,
-                                                false
-                                            )
-                                        }
-                                        className={`${
-                                            !user_applicant.status
-                                                ? "bg-blue-600 text-white"
-                                                : "bg-gray-300 opacity-50 text-black"
-                                        } text-sm shadow-lg hover:opacity-80 transition-opacity duration-300 px-4 py-2 rounded-full`}
-                                    >
-                                        Unapproved
-                                    </button>
-                                </div>
-                            </React.Fragment>
-                        ))}
+        <li
+            className={`sm:flex shadow-sm items-center ${isDarkTheme ? "bg-[#0F1820]" : "bg-[#F0F3FB]"} rounded-3xl p-4 mb-2 w-full`}
+        >
+            <div className="flex sm:relative sm:justify-between w-full">
+                <div className="sm:flex md:flex-row flex-col text-center w-full sm:items-center">
+                    <div className="font-bold w-full">
+                        {applicant.user?.name}
+                    </div>
+                    <div className="font-bold w-full">
+                        {applicant.user?.email.substring(
+                            0,
+                            applicant.user?.email.indexOf("@")
+                        )}
+                    </div>
+                    <div className="font-bold w-full">
+                        {applicant.user?.email}
+                    </div>
+                    <div className="mr-2 mt-2 md:mt-0 flex space-x-2">
+                        <button
+                            onClick={() => handleToggleApprovation(true)}
+                            className={`${
+                                status
+                                    ? "bg-blue-600 text-white"
+                                    : "bg-gray-300 opacity-50 text-black"
+                            } text-sm shadow-lg hover:opacity-80 transition-opacity duration-300 px-4 py-2 rounded-full`}
+                        >
+                            Approved
+                        </button>
+                        <button
+                            onClick={() => handleToggleApprovation(false)}
+                            className={`${
+                                !status
+                                    ? "bg-blue-600 text-white"
+                                    : "bg-gray-300 opacity-50 text-black"
+                            } text-sm shadow-lg hover:opacity-80 transition-opacity duration-300 px-4 py-2 rounded-full`}
+                        >
+                            Unapproved
+                        </button>
                     </div>
                 </div>
+            </div>
 
-                <div
-                    className={
-                        "text-center absolute bottom-10 right-0.5 left-0.5"
-                    }
-                >
-                    <Button
-                        className={"text-center shadow-lg"}
-                        onClick={() => sendData()}
-                    >
-                        Enviar dados
-                    </Button>
-                </div>
-            </li>
-        </>
+            <div className="text-center absolute bottom-10 right-0.5 left-0.5">
+                <Button className="text-center shadow-lg" onClick={sendData}>
+                    Enviar dados
+                </Button>
+            </div>
+        </li>
     );
 }
--- src/app/web/components/Enrolled/EnrolledStudentsList.tsx
+++ src/app/web/components/Enrolled/EnrolledStudentsList.tsx
@@ -2,6 +2,7 @@ import { useThemeDetector } from "@functions/ThemeDetector.ts";
 import "@styles/scrollbar.css";
 import EnrolledStudentCard from "./EnrolledStudentCard.tsx";
 import NoElementsFound from "@components/GenericComponents/NoElementsFound.tsx";
+import { Button } from "primereact/button";
 import IProject from "@interfaces/project/IProject.ts";
 
 interface EnrolledStudentProps {
@@ -13,22 +14,59 @@ export default function EnrolledStudentList({
 }: EnrolledStudentProps) {
     const isDarkTheme = useThemeDetector();
 
+    function handleExportApprovedStudents() {
+        const applicants = students.applicants;
+        if (!applicants) {
+            throw new Error("No applicants found");
+        }
+        const approvedStudents = students.applicants.filter(
+            (student) => student.status
+        );
+        const data = approvedStudents.map((student) => ({
+            name: student.user.name,
+            email: student.user.email,
+            RA: student.user.email.split("@")[0]
+        }));
+
+        const headers = "Nome,RA,E-mail";
+        const csvRows = data.map(
+            (student) => `${student.name},${student.RA},${student.email}`
+        );
+        const csvContent = [headers, ...csvRows].join("\n");
+
+        const blob = new Blob([csvContent], { type: "text/csv" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "approved_students.csv";
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
     return (
         <div
             className={`w-full ml-4 px-7 py-4 relative ${isDarkTheme ? "bg-[#14222E]" : "bg-[#FFFFFF]"} rounded-3xl`}
         >
+            <div className="text-end">
+                <Button
+                    className="mt-1 shadow-md mb-3 right-0"
+                    onClick={handleExportApprovedStudents}
+                >
+                    Export CSV
+                </Button>
+            </div>
             <div>
                 <ul className="w-full max-h-screen pb-48 pe-5 custom-scrollbar overflow-y-auto">
                     {students.applicants[0] !== undefined ? (
-                        students.applicants.map((_, index: number) => (
+                        students.applicants.map((applicant, index) => (
                             <EnrolledStudentCard
                                 key={index}
                                 projectID={students.id}
-                                users_applicants={students.applicants}
+                                applicant={applicant}
                             />
                         ))
                     ) : (
-                        <div className={"w-full text-center"}>
+                        <div className="w-full text-center">
                             <NoElementsFound message="No students were found" />
                         </div>
                     )}
